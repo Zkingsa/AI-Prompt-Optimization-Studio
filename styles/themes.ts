9381b4660e4130a132d0932aaf4e86122edcf629
Zkingsa <161206144+Zkingsa@users.noreply.github.com>
2026-07-02 13:30:31 +0200

    Add files via upload

---
+++ styles/themes.ts
@@ -0,0 +1,12 @@
+export const themes = {
+  light: {
+    background: "#ffffff",
+    foreground: "#0a0a0a",
+  },
+  dark: {
+    background: "#0a0a0a",
+    foreground: "#ffffff",
+  },
+};
+
+export default themes;
