a32581c0aba7fff63ebb95396d8ed5791ef60464
FikaZekhaya <jkdpttz8js@privaterelay.appleid.com>
2026-07-06 14:25:52 +0200

    Remove static export to support dynamic API routes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Removed: output: 'export'
+  // Static export is incompatible with dynamic API routes like /api/api-keys/[id]
+};
 
 module.exports = nextConfig;
